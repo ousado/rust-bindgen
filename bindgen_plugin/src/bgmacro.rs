5adf020e3c46e73ccfa35e08ec703080bc615cd2
Yamakaky <yamakaky@yamaworld.fr>
2016-05-27 22:59:34 +0200

    [plugin] rustfmt

--- bindgen_plugin/src/bgmacro.rs
+++ bindgen_plugin/src/bgmacro.rs
@@ -12,13 +12,16 @@ use syntax::parse::token;
 use syntax::ptr::P;
 use syntax::util::small_vector::SmallVector;
 
-use bindgen::{Bindings, BindgenOptions, LinkType, Logger};
+use bindgen::{BindgenOptions, Bindings, LinkType, Logger};
 use clang_sys;
 
-pub fn bindgen_macro(cx: &mut base::ExtCtxt, sp: codemap::Span, tts: &[ast::TokenTree]) -> Box<base::MacResult+'static> {
+pub fn bindgen_macro(cx: &mut base::ExtCtxt,
+                     sp: codemap::Span,
+                     tts: &[ast::TokenTree])
+                     -> Box<base::MacResult + 'static> {
     let mut visit = BindgenArgsVisitor {
         options: Default::default(),
-        seen_named: false
+        seen_named: false,
     };
 
     visit.options.builtins = true;
@@ -42,12 +45,12 @@ pub fn bindgen_macro(cx: &mut base::ExtCtxt, sp: codemap::Span, tts: &[ast::Toke
     let filename = cx.codemap().span_to_filename(sp);
     let mod_dir = Path::new(&filename).parent().unwrap();
     let cwd = match env::current_dir() {
-      Ok(d)   => d,
-      Err(e)  => panic!("Invalid current working directory: {}", e),
+        Ok(d) => d,
+        Err(e) => panic!("Invalid current working directory: {}", e),
     };
     let p = Path::new(mod_dir);
     if let Err(e) = env::set_current_dir(&p) {
-      panic!("Failed to change to directory {}: {}", p.display(), e);
+        panic!("Failed to change to directory {}: {}", p.display(), e);
     };
 
     // We want the span for errors to just match the bindgen! symbol
@@ -55,29 +58,38 @@ pub fn bindgen_macro(cx: &mut base::ExtCtxt, sp: codemap::Span, tts: &[ast::Toke
     let mut short_span = sp;
     short_span.hi = short_span.lo + codemap::BytePos(8);
 
-    let logger = MacroLogger { sp: short_span, cx: cx };
+    let logger = MacroLogger {
+        sp: short_span,
+        cx: cx,
+    };
 
     let ret = match Bindings::generate(&visit.options, Some(&logger as &Logger), None) {
         Ok(bindings) => {
             // syntex_syntax is not compatible with libsyntax so convert to string and reparse
             let bindings_str = bindings.to_string();
             // Unfortunately we lose span information due to reparsing
-            let mut parser = parse::new_parser_from_source_str(cx.parse_sess(), cx.cfg(), "(Auto-generated bindings)".to_string(), bindings_str);
+            let mut parser = parse::new_parser_from_source_str(cx.parse_sess(),
+                                                               cx.cfg(),
+                                                               "(Auto-generated bindings)"
+                                                                   .to_string(),
+                                                               bindings_str);
 
             let mut items = Vec::new();
             while let Ok(Some(item)) = parser.parse_item() {
                 items.push(item);
             }
 
-            Box::new(BindgenResult { items: Some(SmallVector::many(items)) }) as Box<base::MacResult>
-            
+            Box::new(BindgenResult {
+                items: Some(SmallVector::many(items)),
+            }) as Box<base::MacResult>
+
         }
-        Err(_) => base::DummyResult::any(sp)
+        Err(_) => base::DummyResult::any(sp),
     };
 
     let p = Path::new(&cwd);
     if let Err(e) = env::set_current_dir(&p) {
-      panic!("Failed to return to directory {}: {}", p.display(), e);
+        panic!("Failed to return to directory {}: {}", p.display(), e);
     }
 
     ret
@@ -93,64 +105,81 @@ trait MacroArgsVisitor {
 
 struct BindgenArgsVisitor {
     pub options: BindgenOptions,
-    seen_named: bool
+    seen_named: bool,
 }
 
 impl MacroArgsVisitor for BindgenArgsVisitor {
     fn visit_str(&mut self, mut name: Option<&str>, val: &str) -> bool {
-        if name.is_some() { self.seen_named = true; }
-        else if !self.seen_named { name = Some("clang_args") }
+        if name.is_some() {
+            self.seen_named = true;
+        } else if !self.seen_named {
+            name = Some("clang_args")
+        }
         match name {
             Some("link") => {
                 let parts = val.split('=').collect::<Vec<_>>();
                 self.options.links.push(match parts.len() {
                     1 => (parts[0].to_string(), LinkType::Dynamic),
-                    2 => (parts[1].to_string(), match parts[0] {
-                        "static" => LinkType::Static,
-                        "dynamic" => LinkType::Dynamic,
-                        "framework" => LinkType::Framework,
-                        _ => return false,
-                    }),
+                    2 => {
+                        (parts[1].to_string(),
+                         match parts[0] {
+                            "static" => LinkType::Static,
+                            "dynamic" => LinkType::Dynamic,
+                            "framework" => LinkType::Framework,
+                            _ => return false,
+                        })
+                    }
                     _ => return false,
                 })
-            },
+            }
             Some("match") => self.options.match_pat.push(val.to_string()),
             Some("clang_args") => self.options.clang_args.push(val.to_string()),
             Some("enum_type") => self.options.override_enum_ty = val.to_string(),
-            _ => return false
+            _ => return false,
         }
         true
     }
 
     fn visit_int(&mut self, name: Option<&str>, _val: i64) -> bool {
-        if name.is_some() { self.seen_named = true; }
+        if name.is_some() {
+            self.seen_named = true;
+        }
         false
     }
 
     fn visit_uint(&mut self, name: Option<&str>, _val: u64) -> bool {
-        if name.is_some() { self.seen_named = true; }
+        if name.is_some() {
+            self.seen_named = true;
+        }
         false
     }
 
     fn visit_bool(&mut self, name: Option<&str>, val: bool) -> bool {
-        if name.is_some() { self.seen_named = true; }
+        if name.is_some() {
+            self.seen_named = true;
+        }
         match name {
             Some("allow_unknown_types") => self.options.fail_on_unknown_type = !val,
             Some("builtins") => self.options.builtins = val,
-            _ => return false
+            _ => return false,
         }
         true
     }
 
     fn visit_ident(&mut self, name: Option<&str>, _val: &str) -> bool {
-        if name.is_some() { self.seen_named = true; }
+        if name.is_some() {
+            self.seen_named = true;
+        }
         false
     }
 }
 
 // Parses macro invocations in the form [ident=|:]value where value is an ident or literal
 // e.g. bindgen!(module_name, "header.h", builtins=false, clang_args:"-I /usr/local/include")
-fn parse_macro_opts(cx: &mut base::ExtCtxt, tts: &[ast::TokenTree], visit: &mut MacroArgsVisitor) -> bool {
+fn parse_macro_opts(cx: &mut base::ExtCtxt,
+                    tts: &[ast::TokenTree],
+                    visit: &mut MacroArgsVisitor)
+                    -> bool {
     let mut parser = cx.new_parser_from_tts(tts);
     let mut args_good = true;
 
@@ -167,10 +196,10 @@ fn parse_macro_opts(cx: &mut base::ExtCtxt, tts: &[ast::TokenTree], visit: &mut 
                     if let Err(_) = parser.expect(&token::Eq) {
                         return false;
                     }
-                },
+                }
                 _ => {
                     cx.span_err(span, "invalid argument format");
-                    return false
+                    return false;
                 }
             }
         }
@@ -186,7 +215,7 @@ fn parse_macro_opts(cx: &mut base::ExtCtxt, tts: &[ast::TokenTree], visit: &mut 
                 let ret = match &*val {
                     "true" => visit.visit_bool(as_str(&name), true),
                     "false" => visit.visit_bool(as_str(&name), false),
-                    val => visit.visit_ident(as_str(&name), val)
+                    val => visit.visit_ident(as_str(&name), val),
                 };
                 if !ret {
                     cx.span_err(span, "invalid argument");
@@ -205,33 +234,35 @@ fn parse_macro_opts(cx: &mut base::ExtCtxt, tts: &[ast::TokenTree], visit: &mut 
                             ast::LitKind::Int(i, ast::LitIntType::Unsigned(_)) |
                             ast::LitKind::Int(i, ast::LitIntType::Unsuffixed) => {
                                 visit.visit_uint(as_str(&name), i)
-                            },
-                            ast::LitKind::Int(i, ast::LitIntType::Signed(_)) => visit.visit_int(as_str(&name), i as i64),
+                            }
+                            ast::LitKind::Int(i, ast::LitIntType::Signed(_)) => {
+                                visit.visit_int(as_str(&name), i as i64)
+                            }
                             _ => {
                                 cx.span_err(span, "invalid argument format");
-                                return false
+                                return false;
                             }
                         };
                         if !ret {
                             cx.span_err(span, "invalid argument");
                             args_good = false;
                         }
-                    },
+                    }
                     _ => {
                         cx.span_err(span, "invalid argument format");
-                        return false
+                        return false;
                     }
                 }
             }
         }
 
         if parser.check(&token::Eof) {
-            return args_good
+            return args_good;
         }
 
         if !parser.eat(&token::Comma) {
             cx.span_err(parser.span, "invalid argument format");
-            return false
+            return false;
         }
     }
 }
@@ -240,7 +271,7 @@ fn parse_macro_opts(cx: &mut base::ExtCtxt, tts: &[ast::TokenTree], visit: &mut 
 fn as_str<'a>(owned: &'a Option<String>) -> Option<&'a str> {
     match owned {
         &Some(ref s) => Some(&s[..]),
-        &None => None
+        &None => None,
     }
 }
 
@@ -248,14 +279,14 @@ fn as_str<'a>(owned: &'a Option<String>) -> Option<&'a str> {
 enum QuoteState {
     InNone,
     InSingleQuotes,
-    InDoubleQuotes
+    InDoubleQuotes,
 }
 
 fn parse_process_args(s: &str) -> Vec<String> {
     let s = s.trim();
     let mut parts = Vec::new();
     let mut quote_state = QuoteState::InNone;
-    let mut positions = vec!(0);
+    let mut positions = vec![0];
     let mut last = ' ';
     for (i, c) in s.chars().chain(" ".chars()).enumerate() {
         match (last, c) {
@@ -273,23 +304,23 @@ fn parse_process_args(s: &str) -> Vec<String> {
                 quote_state = QuoteState::InDoubleQuotes;
                 positions.push(i);
                 positions.push(i + 1);
-            },
+            }
             (_, '\"') if quote_state == QuoteState::InDoubleQuotes => {
                 quote_state = QuoteState::InNone;
                 positions.push(i);
                 positions.push(i + 1);
-            },
+            }
             // Match <any>'
             (_, '\'') if quote_state == QuoteState::InNone => {
                 quote_state = QuoteState::InSingleQuotes;
                 positions.push(i);
                 positions.push(i + 1);
-            },
+            }
             (_, '\'') if quote_state == QuoteState::InSingleQuotes => {
                 quote_state = QuoteState::InNone;
                 positions.push(i);
                 positions.push(i + 1);
-            },
+            }
             // Match <any><space>
             // If we are at the end of the string close any open quotes
             (_, ' ') if quote_state == QuoteState::InNone || i >= s.len() => {
@@ -299,7 +330,11 @@ fn parse_process_args(s: &str) -> Vec<String> {
                     let starts = positions.iter().enumerate().filter(|&(i, _)| i % 2 == 0);
                     let ends = positions.iter().enumerate().filter(|&(i, _)| i % 2 == 1);
 
-                    let part: Vec<String> = starts.zip(ends).map(|((_, start), (_, end))| s[*start..*end].to_string()).collect();
+                    let part: Vec<String> = starts.zip(ends)
+                                                  .map(|((_, start), (_, end))| {
+                                                      s[*start..*end].to_string()
+                                                  })
+                                                  .collect();
 
                     let part = part.join("");
 
@@ -317,17 +352,17 @@ fn parse_process_args(s: &str) -> Vec<String> {
 
                 positions.clear();
                 positions.push(i + 1);
-            },
-            (_, _) => ()
+            }
+            (_, _) => (),
         }
         last = c;
     }
     parts
 }
 
-struct MacroLogger<'a, 'b:'a> {
+struct MacroLogger<'a, 'b: 'a> {
     sp: codemap::Span,
-    cx: &'a base::ExtCtxt<'b>
+    cx: &'a base::ExtCtxt<'b>,
 }
 
 impl<'a, 'b> Logger for MacroLogger<'a, 'b> {
@@ -347,7 +382,7 @@ impl<'a, 'b> fmt::Debug for MacroLogger<'a, 'b> {
 }
 
 struct BindgenResult {
-    items: Option<SmallVector<P<ast::Item>>>
+    items: Option<SmallVector<P<ast::Item>>>,
 }
 
 impl base::MacResult for BindgenResult {
@@ -358,11 +393,11 @@ impl base::MacResult for BindgenResult {
 
 #[test]
 fn test_parse_process_args() {
-    assert_eq!(parse_process_args("a b c"),         vec!("a", "b", "c"));
-    assert_eq!(parse_process_args("a \"b\" c"),     vec!("a", "b", "c"));
-    assert_eq!(parse_process_args("a \'b\' c"),     vec!("a", "b", "c"));
-    assert_eq!(parse_process_args("a \"b c\""),     vec!("a", "b c"));
-    assert_eq!(parse_process_args("a \'\"b\"\' c"), vec!("a", "\"b\"", "c"));
-    assert_eq!(parse_process_args("a b\\ c"),       vec!("a", "b c"));
-    assert_eq!(parse_process_args("a b c\\"),       vec!("a", "b", "c\\"));
+    assert_eq!(parse_process_args("a b c"), vec!["a", "b", "c"]);
+    assert_eq!(parse_process_args("a \"b\" c"), vec!["a", "b", "c"]);
+    assert_eq!(parse_process_args("a \'b\' c"), vec!["a", "b", "c"]);
+    assert_eq!(parse_process_args("a \"b c\""), vec!["a", "b c"]);
+    assert_eq!(parse_process_args("a \'\"b\"\' c"), vec!["a", "\"b\"", "c"]);
+    assert_eq!(parse_process_args("a b\\ c"), vec!["a", "b c"]);
+    assert_eq!(parse_process_args("a b c\\"), vec!["a", "b", "c\\"]);
 }
